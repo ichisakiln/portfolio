bbb8dccc5035e81b1d111fd8b10a9f66abd988ad
ichisakiln <releaseanimates@gmail.com>
2024-08-14 19:56:29 +0900

    Add files via upload
    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["*.{html,js}"],
+    theme: {
+      extend: {
+        colors: {
+          'blue-shade': '#33AAEE',
+        },
+      },
+      
+    },
+    plugins: [],
+    
+  }
